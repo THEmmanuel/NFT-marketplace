9b0ab34ec61bf742686893be9a029f9742533791
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-10 02:10:57 +0100

    Add NFT imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './containers/NavBar/NavBar';
+import NFTs from './containers/NFTs/NFTs';
 
 function App() {
   return (
     <div className="App">
 		<NavBar/>
+		<NFTs/>
     </div>
   );
 }
---
+++ src/containers/NFTs/NFTs.js
@@ -0,0 +1,11 @@
+import style from './NFTs.module.css'
+
+const NFTs = () => {
+	return (
+		<section className={style.NFTs}>
+			NFTs
+		</section>
+	)
+}
+
+export default NFTs;
