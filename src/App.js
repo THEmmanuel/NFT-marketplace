8c844a41e320b3bc44405eb074d6b42613e05a2e
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-10 12:01:15 +0100

    Update background color, add classes to app.js and style the navbar a bit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<div className="App">
 			<SidePanel />
-			<section>
+			<section className="ContentContainer">
 				<NavBar />
 				<NFTs />
 			</section>
--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -3,7 +3,17 @@ import style from './NavBar.module.css'
 const NavBar = () => {
 	return (
 		<nav className={style.NavBar}>
-			navbar
+			<input type="search" className={style.NavSearch} />
+
+			<div className={style.NavLinksContainer}>
+				<a href="http://">Explore</a>
+				<a href="http://">Collection</a>
+				<a href="http://">Activity</a>
+				<a href="http://">About</a>
+				<button className={style.ConnectWalletBtn}>
+					Connect wallet
+				</button>
+			</div>
 		</nav>
 	)
 }
