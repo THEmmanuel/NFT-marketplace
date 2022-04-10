0d007bf6bbe0bca7cb912db11f0fa6f0746632fb
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-10 01:49:12 +0100

    Add data

---
+++ src/data.js
@@ -0,0 +1,83 @@
+const data = [{
+		name: '#8520',
+		creator: 'Bored Ape Yatch Club',
+		imageLink: 'https://lh3.googleusercontent.com/ULjfyo4LJhtV3J9K7lu1xh0YZQBa6WHPp-cwlV2C9sUIyTpgSlv554mh_97fRXsziOIu9xwpukl5NQoDbkE3mlXlWR8zU7qcWQsxVg=w600',
+		price: 25,
+		likes: 1200,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8520'
+	},
+
+	{
+		name: 'Shinsekai #336',
+		creator: 'Shinsekai Portal',
+		imageLink: 'https://lh3.googleusercontent.com/PFDUTyYoPu03OcmnjMs_60NcMSpgDl31atQabogDkZ4JQG2x5ow4CWsc7V6UBB-thDhAQezbSkLLarmxKu2o13Zhwv8FLdEXGbEsLQ=w600',
+		price: 0.3706,
+		likes: 598,
+		isVerified: false,
+		openseaLink: 'https://opensea.io/assets/0x98b82d9efc577b1c3aa6578342121231db2b47b9/336'
+	},
+
+
+	{
+		name: 'CryptoPunk #9818',
+		creator: 'CryptoPunks',
+		imageLink: 'https://lh3.googleusercontent.com/vxPRqRqF5XXXiKcY2WmySxPph8n8DIT4-9lUcGPOOf5FDxzODslqcSy5kr5gt8gEI6c20fDmWTMi9CH86GlPo5wuTMbiuFmCPV2q=w600',
+		price: 10,
+		likes: 598,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/9818'
+	},
+
+	{
+		name: 'Azuki #6905',
+		creator: 'Team Azuki',
+		imageLink: 'https://lh3.googleusercontent.com/j2gwFKXI0u32Uh--rwaH5aw2An25ElZ8lQjjJGv-Y_lTqe9ghlq__OW2obGHPiITnA0Le7G9j4yXna6IEp4BTEbx7V5bV7U3wfY9=w600',
+		price: 21,
+		likes: 445,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0xed5af388653567af2f388e6224dc7c4b3241c544/6905'
+	},
+
+	{
+		name: 'CloneX #13245',
+		creator: 'CLONE X - X TAKASHI MURAKAMI',
+		imageLink: 'https://lh3.googleusercontent.com/2vNl-y1PBYpK9vh1PIztepGsfgGQaf76zO14o3VYzw5jl2-gcqpJl0_XAb_vQD0A_yUYtSxlvqrXe2l5tCinOne7Y1fwVV12UmICSA=w600',
+		price: 10,
+		likes: 598,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/13592'
+	},
+
+	{
+		name: 'Kiwami #3078',
+		creator: 'KIWAMI Genesis',
+		imageLink: 'https://lh3.googleusercontent.com/S5y_vwkW5qHErjJap_DDdzL9P49LODHdNoUdL7rzgGr6eewf50VYXHq3vXnkRtaIF9hONy2TBoqJbTRooevxixq_6jpFJjuS2nBTSA=w600',
+		price: 0.65,
+		likes: 6,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0x701a038af4bd0fc9b69a829ddcb2f61185a49568/3078'
+	},
+
+	{
+		name: 'Doodle #8472',
+		creator: 'Doodles',
+		imageLink: 'https://lh3.googleusercontent.com/G4Zf1R86Bgy7CPOuVHrUZujnjsPDdJmhFRggirCvNZ1MNzYWNvd6_qWEuJiuPLNRkClWyv5QMLuXs6yTbdQslzucidPEauMr72YyGA=w600',
+		price: 15.5,
+		likes: 598,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/8472'
+	},
+
+	{
+		name: 'Arcade Land Standard #9733',
+		creator: 'Arcade Land',
+		imageLink: 'https://lh3.googleusercontent.com/WatYAp-_duTyhfa1UQ6yDbm10bw8MA-zkvxoQ7WjrpgceCUfFvzg9-ywGSpwZ4-iVR2pHTBdi0Qikd-yjTrqlmyhpjMis0yJYL__3Q=w600',
+		price: 10,
+		likes: 598,
+		isVerified: true,
+		openseaLink: 'https://opensea.io/assets/0x4a8c9d751eeabc5521a68fb080dd7e72e46462af/9733'
+	},
+]
+
+export default data;
