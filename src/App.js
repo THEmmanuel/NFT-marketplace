ce117c35d6c634d08b5096afc631cf60948085d3
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-10 02:16:41 +0100

    Add sidepanel section style and import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './containers/NavBar/NavBar';
 import NFTs from './containers/NFTs/NFTs';
+import SidePanel from './containers/SidePanel/SidePanel';
 
 function App() {
   return (
     <div className="App">
+		<SidePanel/>
 		<NavBar/>
 		<NFTs/>
     </div>
---
+++ src/containers/SidePanel/SidePanel.js
@@ -0,0 +1,11 @@
+import style from './SidePanel.module.css';
+
+const SidePanel = () => {
+	return (
+		<section className={style.SidePanel}>
+			SidePanel
+		</section>
+	)
+}
+
+export default SidePanel;
