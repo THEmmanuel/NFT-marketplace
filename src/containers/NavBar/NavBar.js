194d820e18b93ef1848e19049bce51d61d19be96
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-11 10:01:49 +0100

    Complete styling the navbar

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -3,16 +3,29 @@ import style from './NavBar.module.css'
 const NavBar = () => {
 	return (
 		<nav className={style.NavBar}>
-			<input type="search" className={style.NavSearch} />
+			<div className={style.NavContainer}>
+				<input
+					type="search"
+					className={style.NavSearch}
+					placeholder='Search by address, author or name' />
 
-			<div className={style.NavLinksContainer}>
-				<a href="http://">Explore</a>
-				<a href="http://">Collection</a>
-				<a href="http://">Activity</a>
-				<a href="http://">About</a>
-				<button className={style.ConnectWalletBtn}>
-					Connect wallet
-				</button>
+				<div className={style.NavLinksContainer}>
+					<a href="http://" className={style.NavLink}>
+						Explore
+					</a>
+					<a href="http://" className={style.NavLink}>
+						Collection
+					</a>
+					<a href="http://" className={style.NavLink}>
+						Activity
+					</a>
+					<a href="http://" className={style.NavLink}>
+						About
+					</a>
+					<button className={style.ConnectWalletBtn}>
+						Connect wallet
+					</button>
+				</div>
 			</div>
 		</nav>
 	)
