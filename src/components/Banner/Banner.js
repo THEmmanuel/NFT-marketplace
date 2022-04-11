87d301a1ca46ffb5af6d06216a26df7ff237c63f
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-04-11 23:56:04 +0100

    Add glass effect and banner text and buttons

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,8 +4,21 @@ import style from './Banner.module.css'
 const Banner = () => {
 	return (
 		<div className={style.Banner}>
-			<img src="https://i.imgur.com/fqRm8em.png" alt="Banner Background" className="style.GlassBackground" />
-			Banner
+			<div className={style.BannerContainer}>
+				<div className={style.BannerContent}>
+					<span className={style.BannerHeading}>
+						Explore and create NFTs.
+					</span>
+					<span className={style.BannerText}>
+						The largest decentralized NFT marketplace in the world.
+					</span>
+
+					<div className={style.ButtonContainer}>
+						<button className={style.PrimaryCTA}>Explore</button>
+						<button className={style.SecondaryCTA}>Create</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 };
